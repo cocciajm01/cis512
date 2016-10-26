147fed9a56fac0b641f2c3771d6f6b2e26fb81c9
JuliaCoccia <juliacoccia@aol.com>
2016-10-25 20:51:17 -0400

    today

--- cis512/__init__.py
+++ cis512/__init__.py
@@ -1,1 +1,23 @@
-text 1+1
+print ('hello world')
+
+
+import csv as csv
+
+town = []
+state= []
+zip = []
+
+
+f = open("/home/JuliaCoccia/Documents/SFHFCLData.csv")
+csv.reader(f)
+for line in f:
+    if "******" not in line:
+        splitline = line.split(',')
+        if len(splitline[0])<20:
+
+            town.append(splitline[0])
+            state.append(splitline[1])
+            zip.append(splitline[2])
+            #print(splitline)
+
+print (town)
